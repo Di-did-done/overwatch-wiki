13192c51c069a9f02631d375bc2b510780947a78
Dkrylova <dkrylova@ptsecurity.com>
2018-04-11 14:48:25 +0300

    Change UpgradeModule to downgradeModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { UpgradeModule } from '@angular/upgrade/static';
-
-import { AppModuleAjs } from './app.module.ajs';
 
 
 @NgModule({
     imports: [
-        BrowserModule,
-        UpgradeModule
+        BrowserModule
     ]
 })
-export class AppModule {
-    constructor(private upgrade: UpgradeModule) {}
-
-    ngDoBootstrap() {
-        this.upgrade.bootstrap(document.body, [AppModuleAjs.name]);
-    }
-}
+export class AppModule {}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,19 @@
+import * as angular from 'angular';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import './polyfills';
 import './vendor';
 
-import { AppModule } from './app';
+import { AppModule, AppModuleAjs } from './app';
+import { downgradeModule } from '@angular/upgrade/static';
+import { StaticProvider } from '@angular/core';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+function bootstrapModule(extra: StaticProvider[]) {
+    return platformBrowserDynamic(extra).bootstrapModule(AppModule);
+}
+
+const downgraded = angular
+    .module('downgraded', [downgradeModule(bootstrapModule)]);
+
+angular.bootstrap(document.body, [AppModuleAjs.name, downgraded.name]);
